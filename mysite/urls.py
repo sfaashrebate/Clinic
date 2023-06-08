c2fac5e701d8070cdaea2adae82879426f63359f
ammaralmasor <ammarend@hotmail.com>
2023-06-08 22:58:42 +0300

    manage and collect static files

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from account.views import *
 
-from drf_yasg.renderers import SwaggerUIRenderer
-# SwaggerUIRenderer.template = 'swagger-ui.html'
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi 
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -44,3 +44,6 @@ urlpatterns = [
 
     path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
