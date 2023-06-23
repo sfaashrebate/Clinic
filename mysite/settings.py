d7cceffb10b6fbe86c0fbf7de2b65267c2612323
sfaashrebate <sfaashrebate@gmail.com>
2023-06-23 16:30:26 +0300

    Add cors headers

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'corsheaders',
     'drf_yasg',
     'django_filters',
 
@@ -57,8 +58,8 @@ MIDDLEWARE = [
     
     'django.middleware.security.SecurityMiddleware',
 
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -167,3 +168,12 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ALLOWED_ORIGINS = [
+# ]
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
+    'https://safaashrebate.pythonanywhere.com',
+)
+# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_CREDENTIALS = True
