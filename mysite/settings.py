a70b5639889ed8ba3559c23489b405328ee8b41a
ola9999 <ola.ms84.99@gmail.com>
2023-08-05 19:13:54 +0300

    'edit'

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,19 +97,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     'default': dj_database_url.parse(
-#         'postgres://sfaa:UfIkjV5njFkF7hQ6s72VY9Q5W77y3TPA@dpg-citk6ip5rnuhcnp8igtg-a.singapore-postgres.render.com/clinic_f7ok'
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        'postgres://sfaa:UfIkjV5njFkF7hQ6s72VY9Q5W77y3TPA@dpg-citk6ip5rnuhcnp8igtg-a.singapore-postgres.render.com/clinic_f7ok'
+    )
+}
 
 # DATABASES = {
 #     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
