f34dc40ee08fb1912f6f9b1de9afb0cf73362676
sfaa-sh <sfaa.clinic@gmail.com>
2023-08-22 16:02:13 +0300

    Add permissions to upload file api

--- filesmanager/views.py
+++ filesmanager/views.py
@@ -4,13 +4,13 @@ from rest_framework.parsers import MultiPartParser
 
 from filesmanager.models import Media#, TestModel
 from filesmanager.serializers import FileUploadSerializer#,TestSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 class FileUploadViewSet(ModelViewSet):
     queryset = Media.objects.all()
     serializer_class = FileUploadSerializer
     parser_classes = [MultiPartParser,]
-
+    permission_classes = [IsAuthenticated]
 
 # class TestViewSet(ModelViewSet):
 #     """
